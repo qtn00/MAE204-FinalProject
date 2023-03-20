34a4f287fe55fc5227f4ac0f14050ab00bc80d13
qtn00 <qtn001@ucsd.edu>
2023-03-20 02:13:56 -0700

    2:15

--- main/main.m
+++ main/main.m
@@ -8,7 +8,7 @@ savepath
 % 	    -1 0 0  -335.6;
 % 	    0 -1 0  237;
 % 	    0 0 0    1];
-T_reference_ini = [0 1 0 247;1 0 0 -169; 0 0 -1 782;0 0 0 1];
+T_reference_ini = [0 1 0 323.6;1 0 0 -335.6; 0 0 -1 237;0 0 0 1];
 % Initial Tsc Input of Wrapper
 T_sc_ini = [1 0 0 450;
     0 1 0 -300;
@@ -52,41 +52,21 @@ end
 
 T_sed = traj;
 T_sedn = traj(1,2:end);
-V_b = zeros(6,length(traj));
-V_error = zeros(6,length(traj));
-thetalist_dot = zeros(6,length(traj));
+[V_b,V_error,thetalist] = FeedbackControl(thetalist,T_sed,T_sedn,kp,ki,dt);
 
-for i = 1:length(traj)-1
-    T_se_current{i} = round(FKinSpace(M,Slist,thetalist(i,:)'),1);
-    V_b(:,i) = round(FeedbackControl(T_se_current{i},T_sed{i},T_sedn{i},kp,ki,dt),5);
-    Jb = round(JacobianBody(Blist,thetalist(i,:)),4);
-    psuedoJb = round(pinv(Jb),4);
-    thetalist_dot(:,i) = round(psuedoJb*V_b(:,i),4);
-    thetalist(i+1,:) = NextState(thetalist(i,:),thetalist_dot(:,i)',dt,10);
-    for i3 = 1:6
-        if thetalist(i+1,i3) > 2*pi || thetalist(i+1,i3) < -2*pi
-            thetalist(i+1,i3) = wrapToPi(thetalist(i+1,i3));
-        end
-    end
-    V_error(:,i) = se3ToVec(round(MatrixLog6(TransInv(T_se_current{i})*T_sed{i}),2));
+    
 
-end
+
 %%
-figure; hold on 
-for a = 1:length(V_error)
-    ang(a) = norm(V_error([1:3],a));
-    lin(a) = norm(V_error(4:6,a));
-end
-plot(0:0.01:(length(ang)*dt)-0.01,ang);
- 
-% plot(1:length(V_error),V_error(1,:));
-% plot(1:length(V_error),V_error(2,:));
-% plot(1:length(V_error),V_error(3,:));
-% plot(1:length(V_error),V_error(4,:));
-% plot(1:length(V_error),V_error(5,:));
-% plot(1:length(V_error),V_error(6,:));
-
-
+figure; hold on;
+plot(V_error');
+% figure; hold on 
+% for a = 1:length(V_error)
+%     ang(a) = norm(V_error([1:3],a));
+%     lin(a) = norm(V_error(4:6,a));
+% end
+% plot(0:0.01:(length(ang)*dt)-0.01,ang);
+%  
 output = zeros(length(thetalist),7);
 for ii = 1:length(thetalist)
     output(ii,:) = [thetalist(ii,:),gripper_state(ii)];
--- main/main2totestfeedbackasafunction.m
+++ main/main2totestfeedbackasafunction.m
@@ -6,25 +6,6 @@ format long g;
 thetalist = [-pi/6,-pi/2,pi/2,-pi/2,-pi/2,5*pi/6];
 T_sed = [0 0 1 300;-1 0 0 -300;0 -1 0 237;0 0 0 1];
 T_sedn = [0 0 1 290;-1 0 0 -290;0 -1 0 237;0 0 0 1];
-kp = 0; ki = 0;
+kp = 1; ki = 0;
 dt = 0.01;
-
-S1 = [0,0,1,-300,0,0]';
-S2 = [0,1,0,-240,0,0]';
-S3 = [0,1,0,-240,0,244]';
-S4 = [0,1,0,-240,0,457]';
-S5 = [0,0,-1,169,457,0]';
-S6 = [0,1,0,-155,0,457]';
-Slist = [S1 S2 S3 S4 S5 S6];
-M = [1 0 0 457; 0 1 0 78; 0 0 1 155; 0 0 0 1];
-Blist = zeros(6,6);
-for i = 1:length(Slist)
-    Blist(:,i) = Adjoint(TransInv(M))*Slist(:,i);
-end
-T_se = FKinSpace(M,Slist,thetalist.');
-
-V_b = FeedbackControl(T_se,T_sed,T_sedn,kp,ki,dt);
-Jb = round(JacobianBody(Blist,thetalist),8);
-psuedoJb = round(pinv(Jb),8);
-thetalist_dot = round(pinv(Jb)*V_b,4);
-V_error = se3ToVec(round(MatrixLog6(TransInv(T_se)*T_sed),2));
+[V_b,V_error,thetalist] = FeedbackControl(thetalist,T_sed,T_sedn,kp,ki,dt);
